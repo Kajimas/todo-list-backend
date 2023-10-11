e95d01487b4828f326790e0f4e27b980697f35cc
William Egge <lockegge@gmail.com>
2023-10-11 17:43:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-list-backend
+A decoupled task list application built with Node.js and Express, showcasing a RESTful API backend and integration with MongoDB
---
+++ app.js
@@ -0,0 +1,65 @@
+// Import the required modules
+const express = require("express"); // Import express module
+const dotenv = require("dotenv"); // Import dotenv module
+// const mongoose = require("mongoose"); // Import mongoose module
+const cors = require("cors"); // Import CORS module
+
+const app = express(); // Initialize the express app
+// const router = express.Router(); // Create a new express router
+
+// Use CORS middleware to enable cross-origin requests
+app.use(cors());
+
+dotenv.config(); // Configure dotenv to load the .env file
+
+// Define the port for the server
+const PORT = process.env.PORT || 3000;
+// const DB_USERNAME = process.env.DB_USERNAME;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+// const DB_NAME = process.env.DB_NAME;
+
+
+// Tests that will be removed later
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get("/testdotenv", (req, res) => {
+  res.send(process.env.TEST_VARIABLE);
+});
+
+// const CONFIG = {
+//     DB_URL: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.sloy5er.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+//     DB_NAME: DB_NAME,
+//   };
+
+// mongoose
+//   .connect(CONFIG.DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB Connected!"))
+//   .catch((err) => {
+//     console.error(`Could not connect to MongoDB Atlas. ${err}`);
+//   });
+
+// // Define the middleware for the express app
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Simple endpoint to test the setup
+// router.get("/hello", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// // Mount the router on the '/api' path
+// app.use("/api", router);
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
