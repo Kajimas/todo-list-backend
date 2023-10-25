5eff1a56943db0a2f459c3445ea1b2c9e90fc621
William Egge <lockegge@gmail.com>
2023-10-25 15:55:14 -0400

    Working DELETE API

--- app.js
+++ app.js
@@ -8,11 +8,26 @@ const swaggerDocument = require("./swagger.json");
 const swaggerUi = require("swagger-ui-express"); // Import swagger-ui-express module
 const mongoose = require("mongoose"); // Import mongoose module
 
+const ObjectId = mongoose.Types.ObjectId; // Import ObjectId type from mongoose
 const app = express(); // Initialize the express app
 
 // Use CORS middleware to enable cross-origin requests
 app.use(cors());
 app.use(express.json());
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message });
+});
+
+// function for simple validation of email
+function extractAndValidateEmail(req, res, next) {
+  const email = req.params.email;
+  if (typeof email !== "string") {
+    return res.status(400).send("Input must be a string!");
+  }
+  req.email = email;
+  next();
+}
 
 // Define the port for the server
 dotenv.config(); // Configure dotenv to load the .env file
@@ -95,10 +110,10 @@ app.get("/", async (req, res) => {
 /**
  * checks todo tasks of a user by email
  */
-app.get("/:email/todo", async (req, res, next) => {
+app.get("/:email/todo", extractAndValidateEmail, async (req, res, next) => {
   try {
-    const email = req.params.email; // Grab the email from the URL parameter
-    const user = await User.findOne({ email: email }); // Search user by the email parameter
+    const user = await User.findOne({ email: req.email }); // Search user by the email parameter
+
     if (!user) {
       return res.status(404).send("User not found");
     }
@@ -112,9 +127,8 @@ app.get("/:email/todo", async (req, res, next) => {
 /**
  * creates new todos for a user by email
  */
-app.post("/:email/todo", async (req, res, next) => {
+app.post("/:email/todo", extractAndValidateEmail, async (req, res, next) => {
   try {
-    const email = req.params.email;
     const taskDetails = req.body;
 
     // Validate task details (you can expand upon this)
@@ -124,7 +138,7 @@ app.post("/:email/todo", async (req, res, next) => {
 
     // Find the user by email and update their 'todo' list
     const user = await User.findOneAndUpdate(
-      { email: email },
+      { email: req.email },
       { $push: { todo: taskDetails } },
       { new: true, useFindAndModify: false } // 'new: true' returns the updated document
     );
@@ -137,6 +151,121 @@ app.post("/:email/todo", async (req, res, next) => {
   } catch (err) {
     console.error(err);
     next(err);
+  }
+});
+
+/**
+ * gets a todo task of a user by the task ID
+ */
+app.get("/:email/todo/:taskId", async (req, res, next) => {
+  try {
+    const { email, taskId } = req.params;
+
+    // Convert taskId to ObjectId
+    const taskObjectId = new mongoose.Types.ObjectId(taskId);
+
+    // Find the user with the given email
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      console.error(`User with email ${email} not found.`);
+      return res.status(404).send(`User with email ${email} not found.`);
+    }
+
+    // Find the specific task using Array#find
+    const task = user.todo.find((t) => t._id.equals(taskObjectId));
+
+    if (!task) {
+      console.error(`Task with ID ${taskId} not found for user ${email}.`);
+      return res
+        .status(404)
+        .send(`Task with ID ${taskId} not found for user ${email}.`);
+    }
+
+    // Return the found task
+    return res.json(task);
+  } catch (err) {
+    console.error(
+      `Error while processing request for user ${req.params.email} and task ${req.params.taskId}: ${err.message}`
+    );
+    next(err);
+  }
+});
+
+/**
+ * Updates a todo task of a user by email
+ */
+app.put("/:email/todo/:id", extractAndValidateEmail, async (req, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const updatedTask = req.body;
+
+    console.log("Target Task ID:", taskId);
+
+    // Validate the updated task details
+    if (!updatedTask.task || updatedTask.completed === undefined) {
+      return res.status(400).send("Invalid task details");
+    }
+
+    const user = await User.findOne({ email: req.email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    console.log("User's Tasks:", user.todo);
+
+    const task = user.todo.id(taskId);
+    if (!task) {
+      return res.status(404).send("Task not found");
+    }
+
+    // Update the task
+    task.set(updatedTask);
+
+    await user.save();
+
+    return res.json(task); // Return the updated task
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+/**
+ * Deletes a todo task of a user by email
+ */
+app.delete("/:email/todo/:taskId", async (req, res, next) => {
+  try {
+      const { email, taskId } = req.params;
+
+      // Convert taskId to ObjectId
+      const taskObjectId = new mongoose.Types.ObjectId(taskId);
+
+      // Find the user with the given email
+      const user = await User.findOne({ email: email });
+      if (!user) {
+          console.error(`User with email ${email} not found.`);
+          return res.status(404).send(`User with email ${email} not found.`);
+      }
+
+      // Find the specific task using Array#find
+      const task = user.todo.find(t => t._id.equals(taskObjectId));
+      if (!task) {
+          console.error(`Task with ID ${taskId} not found for user ${email}.`);
+          return res.status(404).send(`Task with ID ${taskId} not found for user ${email}.`);
+      }
+
+      // Remove the task from the user's todo array
+      const taskIndex = user.todo.indexOf(task);
+      user.todo.splice(taskIndex, 1);
+
+      // Save the user with the task removed
+      await user.save();
+
+      return res.status(204).send(); // 204 No Content, indicates successful deletion
+  } catch (err) {
+      console.error(`Error while processing request for user ${req.params.email} and task ${req.params.taskId}: ${err.message}`);
+      next(err);
   }
 });
 
